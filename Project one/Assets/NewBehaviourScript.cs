94674e8fb26c41f49df8b5ea4037c790141d0a0d
unknown <10761382@CS514-22.ad.uvu.edu>
2015-08-31 10:08:27 -0600

    Functions and Variables Assignment

---
+++ Project one/Assets/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour 
+{
+
+	int charlie = 3;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		charlie= code(charlie);
+		Debug.Log (charlie);
+
+
+	}
+	int code (int wow)
+	{
+		int ret;
+		ret = wow * 5;
+		return ret;
+	}
+
+	    
+
+
+}
